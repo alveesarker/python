1da7be7608e4d2dc7db05d04b4eb635a024424d3
Alvee Sarker <143249362+alveesarker@users.noreply.github.com>
2024-05-23 23:16:03 +0600

    Update hmm.py
    I have added some line of code of simply linked list

--- hmm.py
+++ hmm.py
@@ -100,3 +100,88 @@ print(f"LCM of {a} and {b} is {lcm}")
 print(f"({gcd} X {lcm}) = {gcd * lcm}")
 print(f"({a} X {b}) = {a * b}")
 print(f"Proved ({a} X {b}) = ({gcd} X {lcm})")
+
+
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+
+    def add_begin(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def add_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.next is not None:
+                n = n.next
+            n.next = new_node
+
+    def add_after(self, data, x):
+        n = self.head
+        while n is not None:
+            if x == n.data:
+                break
+            n = n.next
+        if n is None:
+            print("Node is not present in the Linked List.")
+        else:
+            new_node = Node(data)
+            new_node.next = n.next
+            n.next = new_node
+
+    def add_before(self, data, x):
+        if self.head is None:
+            print("The Linked list is empty")
+            return
+        if self.head.data == x:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+            return
+        n = self.head
+        while n.next is not None:
+            if n.next.data == x:
+                break
+            n = n.next
+        if n.next is None:
+            print("Node is not found.")
+        else:
+            new_node = Node(data)
+            new_node.next = n.next
+            n.next = new_node
+
+    def print_ll(self):
+        if self.head is None:
+            print("Linked list is empty.")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data, end='-->')
+                n = n.next
+            print("null")
+
+
+ll1 = LinkedList()
+ll1.add_begin(10)
+ll1.add_begin(5)
+ll1.add_end(20)
+ll1.add_after(15, 10)
+ll1.add_before(18, 20)
+ll1.add_before(3, 5)
+
+ll1.print_ll()
